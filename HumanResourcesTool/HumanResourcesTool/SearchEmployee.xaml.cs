e2f8b04eeea9419cd4585f0374b4aee5d124ee49
DESKTOP-6P33P2L\lroma <Lromanz_ca@siapre.com>
2018-09-09 02:49:19 -0400

    Update Search feature 02:49 2018-09-09 LRZ

--- HumanResourcesTool/HumanResourcesTool/SearchEmployee.xaml.cs
+++ HumanResourcesTool/HumanResourcesTool/SearchEmployee.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HumanResourcesTool.ServiceReference;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -22,10 +25,19 @@ namespace HumanResourcesTool
 
         ServiceReference.HRWebServicesClient HRWebServices;
 
-        public SearchEmployee()
+        public string myOptionSended;
+        public MasterEmployees newFormReceived;
+
+        public SearchEmployee(string value, MasterEmployees formReceived)
         {
             InitializeComponent();
             Loading();
+
+            newFormReceived = formReceived;
+
+            this.myOptionSended = value;
+
+
         }
 
         private void Loading()
@@ -55,16 +67,55 @@ namespace HumanResourcesTool
 
         private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
         {
-            //MasterEmployees editWindow = new MasterEmployees();
-            //var selectedItem = dataGrid1.SelectedItem as sender;
-            //if (selectedItem != null)
-            //    //MessageBox.Show(selectedItem.Emp_EmployeeId.ToString());
-            //    editWindow.Owner = this;
 
-            //editWindow.txtEmployeeId.Text = "1";
+            //MasterEmployees editWindow = new MasterEmployees();
+            MasterEmployees editWindow = newFormReceived;
+            
+            var selectedItem = dataGrid1.SelectedItem as ClassEmployee;
+            if (selectedItem != null)
+                //MessageBox.Show(selectedItem.Emp_EmployeeId.ToString());
+            //editWindow.Owner = this;
+
+            editWindow.flagSearchEmployee = true;
+
+            editWindow.txtEmployeeId.Text = selectedItem.employeeId.ToString();
+
+            if (myOptionSended.ToString() == "u")
+            {
+                editWindow.btnSearchEmployee.IsEnabled = true;
+                editWindow.optionSelectedCRUM = myOptionSended;
+                editWindow.sbItem3.Content = "Update Employee";
+
+                editWindow.txtEmployeeId.IsEnabled = true;
+
+            } else //delete
+            {
+                editWindow.btnSearchEmployee.IsEnabled = true;
+                editWindow.optionSelectedCRUM = myOptionSended;
+                editWindow.sbItem3.Content = "Delete Employee";
+                editWindow.txtEmployeeId.IsEnabled = true;
+                //cbTitles.Focus();
+                //editWindow.txtEmployeeId.Focus();
+
+            }
+
+
+            //------------------------------------------------------------------------
+            //this seccion is to call event txtEmployeeId_KeyDown
+            var kea = new KeyEventArgs(
+                Keyboard.PrimaryDevice,
+                new HwndSource(0, 0, 0, 0, 0, "", IntPtr.Zero), // dummy source
+                0,
+                Key.Enter);
+
+            editWindow.txtEmployeeId_KeyDown(this, kea);
+            //------------------------------------------------------------------------
 
 
             //editWindow.Show();
+
+            Close();
+
         }
 
         private void txtLastName_GotFocus(object sender, RoutedEventArgs e)
@@ -98,11 +149,15 @@ namespace HumanResourcesTool
             if (lastName.Length <= 0) lastName = "";
             if (firstName.Length <= 0) firstName = "";
 
-            HRWebServices = new ServiceReference.HRWebServicesClient();
-
             var query = HRWebServices.GetEmployeesByLastAndFirstName(lastName, firstName);
             dataGrid1.ItemsSource = query;
 
         }
+
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
     }
 }
